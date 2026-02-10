93f62d2e3f28497c995694375dd767fd784b8232
aryansoni25 <ashoksoni9425@gmail.com>
2026-02-10 18:27:46 +0530

    updates

--- src/main/java/org/studyeasy/DoublyLL.java
+++ src/main/java/org/studyeasy/DoublyLL.java
@@ -5,47 +5,62 @@ public class DoublyLL {
     public Node head;
     public Node tail;
     public int size;
-    public Node createNode(int rollNo,String name){
-        Node node=new Node();
-        head=new Node();
-        node.rollNo=rollNo;
-        node.name=name;
-        node.previous=null;
-        node.next=null;
-        head=node;
-        tail=node;
-        size=1;
+
+    public Node createNode(int rollNo, String name) {
+        Node node = new Node();
+        node.rollNo = rollNo;
+        node.name = name;
+        node.previous = null;
+        node.next = null;
+        head = node;
+        tail = node;
+        size = 1;
         return node;
     }
 
-    public Node insertNode(int rollNo,String name,int location){
-        Node node=new Node();
-        node.name=name;
-        node.rollNo=rollNo;
+    public Node insertNode(int rollNo, String name, int location) {
+        Node node = new Node();
+        node.rollNo = rollNo;
+        node.name = name;
 
-        if(location==0){
-            node.next=head;
-            node.previous=null;
-            head.previous=node;
-            head=tail;
-            size++;
-        }
-        return node;
-    }
-    public void reverse(){
-        if(head!=null){
-            Node temp=tail;
-            int index=0;
-            while (index<size){
-                System.out.print(temp.rollNo+" "+temp.name+" ");
-                temp=temp.previous;
-
+        if (head == null) {
+            return createNode(rollNo, name);
+        } else if (location == 0) {
+            node.next = head;
+            node.previous = null;
+            head.previous = node;
+            head = node;
+        } else if (location >= size) {
+            node.next = null;
+            node.previous = tail;
+            tail.next = node;
+            tail = node;
+        } else {
+            Node temp = head;
+            int index = 0;
+            while (index < location - 1) {
+                temp = temp.next;
                 index++;
             }
-        }else {
+            node.next = temp.next;
+            node.previous = temp;
+            temp.next.previous = node;
+            temp.next = node;
+        }
+        size++;
+        return node;
+    }
+
+    public void reverse() {
+        if (head != null) {
+            Node temp = tail;
+            while (temp != null) {
+                System.out.print(temp.rollNo + " " + temp.name + " ");
+                temp = temp.previous;
+            }
+        } else {
             System.out.println("DLL does not exist");
         }
         System.out.println("\n");
     }
-
 }
--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         DoublyLL dl=new DoublyLL();
         dl.createNode(101,"Aryan");
-        dl.insertNode(102,"Aishwary",0);
+        dl.insertNode(102,"Aishwary",1);
         dl.reverse();
     }
 }
